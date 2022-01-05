a7137f3b7ffe5b667c3799f293cbfcea12d35c19
Enrique <enriquesamudio1999@gmail.com>
2022-01-05 18:19:00 -0300

    Desktop done

---
+++ js/scripts.js
@@ -0,0 +1,78 @@
+const tabs = document.querySelectorAll('.tab');
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    tabs.forEach(tab => {
+        tab.addEventListener("click", loadData);
+    });
+
+
+});
+
+const loadData = async (e) => {
+
+    const time = e.target.dataset.time;
+
+    try {
+        const url = "../data.json";
+
+        const res = await fetch(url);
+        const data = await res.json();
+        
+        changeActiveTab(e);
+        changeData(data, time);
+        
+
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+const changeData = (dataArray, time) => {
+
+    dataArray.forEach(data => {
+
+        let cardTimeData;
+        let cardPreviousData;
+        let cardTimeInfoData;
+
+        if (time === "daily") {
+            cardTimeData = data.timeframes.daily.current;
+            cardPreviousData = data.timeframes.daily.previous;
+            cardTimeInfoData = "Day";
+        }
+
+        if (time === "weekly") {
+            cardTimeData = data.timeframes.weekly.current;
+            cardPreviousData = data.timeframes.weekly.previous;
+            cardTimeInfoData = "Week";
+        }
+
+        if (time === "monthly") {
+            cardTimeData = data.timeframes.monthly.current;
+            cardPreviousData = data.timeframes.monthly.previous;
+            cardTimeInfoData = "Month";
+        }
+
+        const card = document.querySelector(`[data-card="${data.title}"]`);
+
+        const cardTime = card.querySelector(".card-time");
+        const cardPreviousTime = card.querySelector(".card-previous-time");
+
+        cardTime.innerText = `${cardTimeData}${cardTimeData == 1 ? "hr" : "hrs"}`;
+        cardPreviousTime.innerText = `Last ${cardTimeInfoData} - ${cardPreviousData}${cardPreviousData == 1 ? "hr" : "hrs"}`;
+
+    });
+
+}
+
+const changeActiveTab = (e) => {
+
+    tabs.forEach(tab => {
+        tab.classList.remove("active");
+    });
+
+    e.target.classList.add("active");
+
+}
